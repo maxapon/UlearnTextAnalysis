d6434bbb3faff2a083f539f3b5f5de5d1c03f7e2
maxaponpatapon@gmail.com <maxaponpatapon@gmail.com>
2022-09-26 22:33:10 +0300

    complited last task

--- TextGeneratorTask.cs
+++ TextGeneratorTask.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace TextAnalysis
 {
@@ -9,7 +11,34 @@ namespace TextAnalysis
             string phraseBeginning,
             int wordsCount)
         {
-            return phraseBeginning;
+            StringBuilder sb = new StringBuilder(phraseBeginning);
+            var (lastWords, lastWordsCount) = GetLastWords(phraseBeginning);
+            for (int i = 0; i < wordsCount; i++)
+            {
+                string key = String.Empty;
+                if (lastWordsCount == 2 && nextWords.ContainsKey(lastWords))
+                    key = lastWords;
+                else
+                {
+                    string oneLastWord = lastWordsCount == 1 ? lastWords : lastWords.Split()[1];
+                    if (nextWords.ContainsKey(oneLastWord))
+                        key = oneLastWord;
+                    else
+                        break;
+                }
+                sb.Append(" ");
+                sb.Append(nextWords[key]);
+                (lastWords, lastWordsCount) = GetLastWords(sb.ToString());
+            }
+            return sb.ToString();
+        }
+
+        private static (string, int) GetLastWords(string phrase, char separator =  ' ')
+        {
+            var allWords = phrase.Split(separator);
+            return allWords.Length >= 2 ? 
+                    ($"{allWords[allWords.Length - 2]} {allWords[allWords.Length - 1]}", 2) :
+                    (allWords[0], 1);
         }
     }
 }
